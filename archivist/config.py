b79de6341a7763cdaff5c8b9782fc1bdf7184801
tlazizbek <tlazizbek@proton.me>
2026-08-21 15:35:12 +0500

    install dependencies and configure the environment

---
+++ archivist/config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LLM_API_KEY = os.getenv("LLM_API_KEY", "")
+LLM_BASE_URL = os.getenv("LLM_BASE_URL", "")
+DB_PATH = os.getenv("DB_PATH", './archivist.db')
